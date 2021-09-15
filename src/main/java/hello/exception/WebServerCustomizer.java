a003ed932b47bde456c2ccf2a327715e410f54ea
byeungoo <dksekfldks65@naver.com>
2021-09-15 20:45:06 +0900

    exception 페이지 메커니즘 이해

---
+++ src/main/java/hello/exception/WebServerCustomizer.java
@@ -0,0 +1,26 @@
+package hello.exception;
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+//@Component
+public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+
+    /**
+     * 스프링 부트 뜰 때 톰캣 서버한테 에러 페이지 등록해줌.
+     * WAS가 이 경로를 다시 호출 해줌줌     * @param factory
+     */
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
+        ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
+
+        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500");
+
+        factory.addErrorPages(errorPage404, errorPage500, errorPageEx);
+    }
+
+}
---
+++ src/test/java/hello/exception/DemoProjectForSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.exception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjectForSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
